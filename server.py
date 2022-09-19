d9c5e1aac7d1583bd04e8ccfcc4f938139ec044d
Sergei Pishchulenok <sapishchulenok@domclick.ru>
2022-09-19 22:32:46 +0300

    First commit

--- server.py
+++ server.py
@@ -1,21 +1,49 @@
+from re import A
 from aiohttp import web
 import aiofiles
+import asyncio
+import os
 
 
-async def archive(request):
-    raise NotImplementedError
+async def archive_handler(request: web.Request):
+    
+    response = web.StreamResponse()
+
+    response.headers['Content-Type'] = 'text/html'
+    response.headers['Content-Disposition'] = 'attachment;'\
+                                              'filename="archive.zip"'
+
+    await response.prepare(request)
+    archive_hash = request.match_info['archive_hash']
+    cmd = f"zip -r - ./"
+    proc = await asyncio.create_subprocess_shell(
+        cmd=cmd,
+        cwd = f'{os.getcwd()}/test_photos/{archive_hash}',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+
+    # 512 kbytes = 524288 bytes
+    while True:
+        if proc.stdout.at_eof():
+            break
+        stdout = await proc.stdout.read(n=300)
+        await response.write(stdout)
+        
+    return response
 
 
-async def handle_index_page(request):
+async def index_page_handler(request):
     async with aiofiles.open('index.html', mode='r') as index_file:
         index_contents = await index_file.read()
     return web.Response(text=index_contents, content_type='text/html')
 
 
 if __name__ == '__main__':
+    print(os.getcwd())
     app = web.Application()
     app.add_routes([
-        web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archive),
+        web.get('/', index_page_handler),
+        web.get('/archive/{archive_hash}/', archive_handler),
     ])
     web.run_app(app)
